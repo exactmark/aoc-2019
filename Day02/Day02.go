4d0ab612df5898b26762d8e30fdd5da776abf6d6
Mark Fowler <exactmark@gmail.com>
2021-11-28 11:59:35 -0500

    Completed Day02 pt 1

--- Day02/Day02.go
+++ Day02/Day02.go
@@ -6,6 +6,93 @@ import (
 	"strings"
 )
 
+type Computer struct {
+	CurrentPtr   int
+	Instructions *[]int
+	Message      string
+}
+
+func (c *Computer) printState() {
+	fmt.Printf("ptr at %v\n", c.CurrentPtr)
+	fmt.Printf("instr: %v\n", c.Instructions)
+	if c.Message != "" {
+		fmt.Printf("instr: %v\n", c.Message)
+	}
+}
+
+func (c *Computer) canProcess() bool {
+	if c.CurrentPtr >= len(*(c.Instructions)) {
+		return false
+	}
+	if !isKnownOp((*(c.Instructions))[c.CurrentPtr]) {
+		return false
+	}
+	return true
+}
+
+func (c *Computer) processNextOp() {
+	nextOp:= (*c.Instructions)[c.CurrentPtr]
+	if nextOp == 1 {
+		c.processAdd()
+	} else if nextOp ==2{
+		c.processMuli()
+	} else if nextOp == 99{
+		c.Message = fmt.Sprintf("completed at ptr %v" ,c.CurrentPtr)
+		c.CurrentPtr=len(*(c.Instructions))
+	}
+
+}
+
+func (c *Computer) processAdd() {
+	if c.CurrentPtr+3> len((*c.Instructions)){
+		c.Message = fmt.Sprintf("illegal add at ptr %v" ,c.CurrentPtr)
+		c.CurrentPtr=len(*(c.Instructions))
+		return
+	}
+	for i := 1; i <= 3; i++ {
+		opPtr:= (*c.Instructions)[c.CurrentPtr+i]
+		if opPtr > len(*(c.Instructions)) {
+			c.Message = fmt.Sprintf("illegal add at ptr %v" ,c.CurrentPtr)
+			c.CurrentPtr=len(*(c.Instructions))
+			return
+		}
+	}
+
+	theSum:=(*c.Instructions)[(*c.Instructions)[c.CurrentPtr+1]]+(*c.Instructions)[(*c.Instructions)[c.CurrentPtr+2]]
+	(*c.Instructions)[(*c.Instructions)[c.CurrentPtr+3]]=theSum
+	c.CurrentPtr+=4
+}
+
+func (c *Computer) processMuli() {
+	if c.CurrentPtr+3> len((*c.Instructions)){
+		c.Message = fmt.Sprintf("illegal muli at ptr %v" ,c.CurrentPtr)
+		c.CurrentPtr=len(*(c.Instructions))
+		return
+	}
+	for i := 1; i <= 3; i++ {
+		opPtr:= (*c.Instructions)[c.CurrentPtr+i]
+		if opPtr > len(*(c.Instructions)) {
+			c.Message = fmt.Sprintf("illegal muli at ptr %v" ,c.CurrentPtr)
+			c.CurrentPtr=len(*(c.Instructions))
+			return
+		}
+	}
+
+	theSum:=(*c.Instructions)[(*c.Instructions)[c.CurrentPtr+1]]*(*c.Instructions)[(*c.Instructions)[c.CurrentPtr+2]]
+	(*c.Instructions)[(*c.Instructions)[c.CurrentPtr+3]]=theSum
+	c.CurrentPtr+=4
+}
+
+func isKnownOp(theOp int) bool {
+	knownOps := [3]int{1, 2, 99}
+	for _, v := range knownOps {
+		if theOp == v {
+			return true
+		}
+	}
+	return false
+}
+
 func solvePt1(inputLines []string) {
 
 	inputParts := strings.Split(inputLines[0], ",")
@@ -17,23 +104,39 @@ func solvePt1(inputLines []string) {
 		intArray[k] = singleInt
 	}
 
-	fmt.Printf("%v\n",intArray)
+	fmt.Printf("%v\n", intArray)
 
-	//fuelSum := 0
-	//
-	//for _, singleLine := range inputLines {
-	//	singleMass, _ := strconv.Atoi(singleLine)
-	//	fuelAmt := getFuel(singleMass)
-	//	fuelSum += fuelAmt
-	//	fmt.Printf("mass: %v, fuel: %v\n", singleMass, fuelAmt)
-	//}
-	//
-	//fmt.Printf("Sum is %v\n", fuelSum)
+	shipPc := Computer{
+		CurrentPtr:   0,
+		Instructions: &intArray,
+	}
 
+	for shipPc.canProcess() {
+		shipPc.processNextOp()
+		shipPc.printState()
+	}
 }
 
 func solvePt2(inputLines []string) {
+	inputParts := strings.Split(inputLines[0], ",")
 
+	intArray := make([]int, len(inputParts))
+
+	for k, v := range inputParts {
+		singleInt, _ := strconv.Atoi(v)
+		intArray[k] = singleInt
+	}
+
+
+	shipPc := Computer{
+		CurrentPtr:   0,
+		Instructions: &intArray,
+	}
+
+	for shipPc.canProcess() {
+		shipPc.processNextOp()
+		shipPc.printState()
+	}
 }
 
 func Solve(inputLines []string) {
